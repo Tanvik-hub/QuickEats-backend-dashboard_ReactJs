7666f7998477e7e933a0b44d08d0d338e7de4ea2
Tanvik-hub <tanvikreddy24@gmail.com>
2024-06-19 21:19:06 +0530

    Update Login.jsx

--- src/vendorDashboard/components/forms/Login.jsx
+++ src/vendorDashboard/components/forms/Login.jsx
@@ -26,14 +26,14 @@ const Login = ({showWelcomeHandler}) => {
       const vendorId=data.vendorId;
       console.log("checking for vendor id=",vendorId);
       const vendorResponse=await fetch(`${API_URL}/vendor/single-vendor/${vendorId}`);
-      window.location.reload()
       const vendorData=await vendorResponse.json();
       if(vendorResponse.ok){
         const vendorFirmId=vendorData.vendorFirmId;
         const vendorFirmName=vendorData.vendor.firm[0].firmName;
         
         localStorage.setItem('firmId',vendorFirmId);
-        localStorage.setItem('firmName',vendorFirmName)
+        localStorage.setItem('firmName',vendorFirmName);
+        window.location.reload();
         
       }
 
